e38c912a679bca54003825ef384a3e7772bea258
Sergio <52253089+DurdenTyler2008@users.noreply.github.com>
2020-09-25 20:26:02 +0300

    Lession_3

--- Lession_3/train.rb
+++ Lession_3/train.rb
@@ -12,3 +12,89 @@
 Может перемещаться между станциями, указанными в маршруте. Перемещение возможно вперед и назад, но только на 1 станцию за раз.
 Возвращать предыдущую станцию, текущую, следующую, на основе маршрута
 =end
+
+class Train
+  attr_accessor :train, :type, :number_cars, :speed, :route, :current_station 
+    
+  
+  def initialize (train, type, number_cars)
+    @train = train
+    @type = type
+    @number_cars = number_cars
+    @speed = 0
+  end
+  
+  def speed=(speed) # набирает скорость(метод сеттер train.speed=100)
+    @speed = speed
+  end
+  
+  def show_speed # возвращает скорость
+    puts "Теущая скорость: #{@speed}"
+  end
+  
+  def brake_speed # тормозит, сбрасывает скорость до 0
+    @speed = 0
+    puts "Поезд стоит"
+  end
+  
+  def show_number_cars # возвращает количество вагонов
+    puts "Поезд имеет #{@number_cars} вагонов"
+  end
+  
+  def add_car # прицепляет 1 вагон
+    @number_cars += 1 if @speed == 0
+  end
+  
+  def send_car # отцепляет 1 вагон
+    if @number_cars == 0
+      puts "У поезда нет вагонов"
+    else
+      @number_cars -= 1 if @speed == 0
+    end
+  end
+  
+  def accept_route(route)
+    @route = route # принимает маршрут следования (объект класса Route)
+    @current_station = route.first_station # При назначении маршрута поезду, поезд автоматически помещается на первую станцию в маршруте
+  end
+  
+  def current_station_index # возвращает индекс текущей станции
+    @route.stations.index(@current_station) 
+  end
+
+  def next_station # возвращает объект следующей станци
+    @route.stations[@route.stations.index(@current_station) + 1]
+  end
+
+  def previous_station # возвращает объект предидущей станции
+    @route.stations[@route.stations.index(@current_station) - 1]
+  end
+
+
+  def moving_forvard
+    if @current_station != @route.end_station
+      @current_station = next_station
+      puts "Вы на станции: #{next_station}" 
+    else
+      puts "Вы на последней станции"
+    end
+  end
+
+  def moving_backward
+    if @current_station != @route.first_station
+      @current_station = previous_station
+      puts "Вы на станции: #{previous_station}"
+    else
+      puts "Вы на первой станции"
+    end
+  end
+
+end
+ 
+    
+  
+
+  
+
+
+
