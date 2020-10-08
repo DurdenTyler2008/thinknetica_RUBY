119f072b8b4da7b50a7f246f5d16d714cd5a5ca4
Sergio <52253089+DurdenTyler2008@users.noreply.github.com>
2020-10-08 11:12:31 +0300

    Create main.rb
    Lession_4

---
+++ Lession_4/main.rb
@@ -0,0 +1,31 @@
+require_relative "station"
+require_relative "route"
+require_relative "train2"
+require_relative "train_pass"
+require_relative "train_cargo"
+
+first_station = Station.new("London")
+end_station = Station.new("Moscow")
+
+train1 = Train.new(123,"pass",15)
+train2 = Train.new(321,"cargo", 18)
+train3 = Train.new(231,"pass", 12)
+
+first_station.add_train(train1)
+first_station.add_train(train2)
+first_station.add_train(train3)
+
+train1.show_number_cars
+first_station.show_type_trains("pass")
+
+
+#route = Route.new(first_station, end_station)
+#route.add_station("Paris")
+#route.add_station("Madrid")
+#route.show_stations
+
+#train1.accept_route(route)
+#train1.previous_station
+#train1.next_station
+#train1.moving_forvard
+#train1.moving_backward
